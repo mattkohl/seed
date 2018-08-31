82257fbeac0f1a5ba5e3fb1da7bcf5988a59e461
mattkohl <hlaford@gmail.com>
2018-08-31 18:15:05 +0100

    +kafka

--- src/run.py
+++ src/run.py
@@ -3,20 +3,28 @@ from pprint import pprint
 from src.spot.playlists import SpotPlaylist
 from src.geni.utils import GenUtils
 from src.geni.parser import GenParser
+from src.bus import Producer
 
 
 def main():
+    sp = SpotPlaylist()
+    gp = GenParser()
+    kp = Producer()
+    producer = kp.connect()
+
     test_uri = "spotify:user:matt.kohl-gb:playlist:2d1Q2cY735lRoXC8cC6DDJ"
-    pg = SpotPlaylist()
-    tracks = pg.extract_tracks(test_uri)
+    kp.publish_message(producer, "playlist", "hh", test_uri)
+
+    tracks = sp.extract_tracks(test_uri)
     track = random.choice(tracks)
+
     artist, song_title = track["artists"][0]["name"], track["title"]
     test_url = GenUtils.link(artist, song_title)
-    pprint(track)
-    print(test_url)
-    gp = GenParser()
     lyrics = gp.download(test_url)
-    print(lyrics)
+    if lyrics is not None:
+        track.update({"genUrl": test_url, "lyrics": lyrics})
+        pprint(track)
+        kp.publish_message(producer, "track", "hh", str(track))
 
 
 if __name__ == "__main__":
