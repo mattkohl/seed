e601754a22f0e9df3220bc04189a4e3cc4a98dad
mattkohl <hlaford@gmail.com>
2018-09-12 18:42:57 +0100

    config update

--- config.py
+++ config.py
@@ -33,24 +33,9 @@ class DockerConfig(Config):
         app.logger.addHandler(file_handler)
 
 
-class UnixConfig(Config):
-
-    @classmethod
-    def init_app(cls, app) -> None:
-        Config.init_app(app)
-
-        # log to syslog
-        import logging
-        from logging.handlers import SysLogHandler
-        syslog_handler = SysLogHandler()
-        syslog_handler.setLevel(logging.INFO)
-        app.logger.addHandler(syslog_handler)
-
-
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'docker': DockerConfig,
-    'unix': UnixConfig,
     'default': DevelopmentConfig
 }
