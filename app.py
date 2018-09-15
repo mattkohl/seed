3ca1f899c68ac462b2a81636791b92ed56086733
mattkohl <hlaford@gmail.com>
2018-09-15 09:43:25 +0100

    logging fix

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import os
-import logging
-from logging import StreamHandler
 
 from dotenv import load_dotenv
 from flask import Flask
@@ -12,13 +10,9 @@ from bus import Producer
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
-log_formatter = '[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s'
-handler = StreamHandler()
-handler.formatter(logging.Formatter(log_formatter, '%m-%d %H:%M:%S'))
-handler.setLevel(logging.INFO)
 
 application = Flask(__name__)
-application.logger.addHandler(handler)
+
 
 kp = Producer()
 producer = kp.connect()
