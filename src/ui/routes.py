38e23770b041ef2e7afa0c727acd0c327597923e
mattkohl <hlaford@gmail.com>
2019-07-19 16:51:46 +0200

    check-in

--- src/ui/routes.py
+++ src/ui/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, url_for, g
 from werkzeug.utils import redirect
 
+from src.forms import SearchForm
 from src.ui import ui
 
 from src.models import Track, Album
@@ -22,9 +23,13 @@ def index():
     return render_template('ui/index.html', songs=songs, offset=offset, song_count=song_count, start_page=start_page, end_page=end_page)
 
 
-@ui.route("/get_song")
-def get_song():
-    return "bar"
+@ui.route("/<song_id>")
+def get_song(song_id: int):
+    q = request.args.get('q') if request.args.get('q') else ""
+    song = Track.query.filter_by(id=song_id).first()
+    if song:
+        return render_template('ui/song.html', song_id=song_id, result=song, q=q)
+    return redirect(url_for('.index'))
 
 
 @ui.route("/edit_song")
@@ -36,7 +41,7 @@ def edit_song():
 def search():
     if not g.search_form.validate_on_submit():
         return redirect(url_for('.index'))
-    return redirect(url_for('.search-results', query=g.search_form.search.data))
+    return redirect(url_for('.search_results', query=g.search_form.search.data))
 
 
 @ui.route('/search-results/<query>')
@@ -51,3 +56,8 @@ def search_results(query, in_xml=True):
         return render_template('ui/xml_search_results.html', query=query, results=result.all(), number=result.count())
     else:
         return render_template('ui/search_results.html', query=query, results=result.all(), number=result.count())
+
+
+@ui.before_request
+def before_request():
+    g.search_form = SearchForm()
